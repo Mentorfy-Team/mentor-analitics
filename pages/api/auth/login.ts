5b8f8980605e157a163bdde3d141a6ee03503077
Alan Jones Rios <alan.jones.rios@gmail.com>
2023-04-18 10:10:05 -0300

    feat: redis removed

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -7,10 +7,7 @@ import {
   createSecureToken,
   methodNotAllowed,
 } from 'next-basics/dist/esm';
-import redis from '@umami/redis-client';
 import { getUser } from 'queries';
-import { setAuthKey } from 'lib/auth';
-
 export interface LoginRequestBody {
   username: string;
   password: string;
@@ -32,12 +29,6 @@ export default async (req: any, res: NextApiResponse<LoginResponse>) => {
     const user = await getUser({ username }, { includePassword: true });
 
     if (user && checkPassword(password, user.password)) {
-      if (redis.enabled) {
-        const token = await setAuthKey(user);
-
-        return ok(res, { token, user });
-      }
-
       const token = createSecureToken({ userId: user.id }, '2fe5f26456cdc87007e7adf0ae7da3ad');
 
       return ok(res, {
